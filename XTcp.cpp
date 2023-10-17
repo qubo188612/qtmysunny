8d18b738581f495517d93d1a902398c2aa048b61
qubo188612 <346386694@qq.com>
2023-10-17 10:43:35 +0800

    first commit

--- XTcp.cpp
+++ XTcp.cpp
@@ -107,7 +107,7 @@ bool XTcp::SetSentBufferlong(int bufferlong) //设置发送缓冲长度
 {
     int sendBufLen = bufferlong;
     int rc;
-    setsockopt(tsock,SOL_SOCKET, SO_SNDBUF,(const char*)&sendBufLen, sizeof(int));
+    rc=setsockopt(tsock,SOL_SOCKET, SO_SNDBUF,(const char*)&sendBufLen, sizeof(int));
     if(rc!=0)
     {
         return true;
